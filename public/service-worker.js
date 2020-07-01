2fabd1a5341d9e40015eb175e709e7b0c005e62f
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-06-30 20:52:28 -0500

    final push

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,12 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
   '/manifest.webmanifest',
-  '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-
-  '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-512x512.png',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/db.js',
+  '/index.js',
 
 
 
