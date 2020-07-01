3e807c97e4956481c3c3a111ee2f494d9d37a256
Derrick Chargois <36555200+dchargois23@users.noreply.github.com>
2020-06-30 21:20:04 -0500

    final push /fingers crossed

--- server.js
+++ server.js
@@ -13,26 +13,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
-
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/expense", {
   useNewUrlParser: true,
   useFindAndModify: false
